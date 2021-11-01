791a480d1c448a0edefb0a085cd846d7ec8e58c3
ahmdsya <syarifahmad817@gmail.com>
2021-11-01 16:33:32 +0700

    bug update

--- resources/views/auth/component/master.blade.php
+++ resources/views/auth/component/master.blade.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
   <meta name="csrf-token" content="{{ csrf_token() }}">
-  <title>{{$title}} | KeepKas</title>
+  <title>Login | KeepKas</title>
 
   <!-- General CSS Files -->
   <link rel="stylesheet" href="{{ asset('template/assets/modules/bootstrap/css/bootstrap.min.css') }}">
